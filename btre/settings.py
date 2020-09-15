e815a7f909c2b47b8445b32a128d4e113535bb71
sw-nowruz <sw@nowruz.group>
2020-09-15 20:19:04 +0430

    Listings app URLs and Template.

--- btre/settings.py
+++ btre/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'pages.apps.PagesConfig',
+    'django.contrib.staticfiles',    
 ]
 
 MIDDLEWARE = [
